09f78c0b331c6fd7ef5b6eb37acfc817fc03a7c1
aslpls <kasikas.ako@gmail.com>
2025-02-08 16:06:52 +0400

    Add files via upload

---
+++ NciksServ/xloginserv.tcl
@@ -0,0 +1,38 @@
+## Description:  
+## Simplified Auto Login X TCL script
+## by aslpls
+
+# Goger Identify TCL
+putlog "Auto X-Login TCL loaded - aslpls"
+
+### Setup
+# Set nickname and password for login - edit as needed
+set nickname "aslhost"
+set password "Abc@123#"
+set login "x@channels.undernet.org"  ;# Server address to use
+
+### Helper function for common actions
+
+proc send_command {command} {
+    global nickname password login
+    putquick "PRIVMSG $login :$command $nickname $password"
+}
+
+# Bind commands to functions
+bind pub m .login  do_login
+
+
+### Procedure Definitions
+
+# Manual Login X
+proc do_login {nick host handle chan text} {
+    send_command "login"
+    putserv "NOTICE $nick :Authenticating to $login..."
+}
+
+
+### Initial Setup
+set init-server {  
+    send_command "login"
+    putserv "MODE $nick +x"
+}
